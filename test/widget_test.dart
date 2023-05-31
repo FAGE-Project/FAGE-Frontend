50ed7b5be8cd614a0e6902ed35082519c7e55352
Helon21 <helonbentes51@hotmail.com>
2023-05-31 15:10:46 -0300

    Implementando validações de cliente

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:fage_telas/login.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fage_telas/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    /*await tester.pumpWidget(const MyApp());*/
+    await tester.pumpWidget(const Login());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
