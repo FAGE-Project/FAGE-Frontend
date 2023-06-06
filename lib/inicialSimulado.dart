fb46a01bda37d38758e3958e0177b8c999b47c09
Helon21 <helonbentes51@hotmail.com>
2023-06-06 15:37:53 -0300

    Update/Ajuste de tamanho da imagem da tela simulada

--- lib/inicialSimulado.dart
+++ lib/inicialSimulado.dart
@@ -10,11 +10,12 @@ class InicialSimulado extends StatelessWidget {
       body: ListView(
         children: [
           legenda(
-            'https://images.ctfassets.net/hrltx12pl8hq/1fR5Y7KaK9puRmCDaIof7j/09e2b2b9eaf42d450aba695056793607/vector1.jpg?fit=fill&w=600&h=400', 
-            'foto para não ficar sem nada',
+            'https://images.ctfassets.net/hrltx12pl8hq/1fR5Y7KaK9puRmCDaIof7j/09e2b2b9eaf42d450aba695056793607/vector1.jpg?fit=fill&w=600&h=400',
+            'foto para não ficar sem nada na tela',
+             220,
+             220,
              (){
-              
-             
+
              },
           ),
         ],
@@ -22,15 +23,19 @@ class InicialSimulado extends StatelessWidget {
     );
   }
 
-  Widget legenda(String url, String legenda, void Function() onTap) {
+  Widget legenda(String url, String legenda, double width, double height, Function() onTap) {
     return InkWell(
       onTap: onTap,
       child: Column(
         children: [
-          Image.network(url),
+          Image.network(
+            url,
+            width: width,
+            height: height,
+          ),
           Text(legenda),
         ],
-        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
       ),
     );
   }
