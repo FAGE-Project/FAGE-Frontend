181b78c84ac091a4c3ba4560df36644e129672e7
Leonardo <leonardosarto1@hotmail.com>
2023-08-29 18:45:31 -0300

    Desenvolvimento da tela inicial do app

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,85 +9,114 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var currentPageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          currentIndex: 0,
-          items: [
-            const BottomNavigationBarItem(
-                icon: Icon(Icons.favorite_outline), label: "Meu Fage"),
-            const BottomNavigationBarItem(
-                icon: Icon(Icons.search), label: "Pesquisar"),
-            const BottomNavigationBarItem(
-                icon: Icon(Icons.calendar_today_outlined),
-                label: "Agendamentos"),
-            const BottomNavigationBarItem(
-                icon: Icon(Icons.person_outline), label: "Perfil"),
-          ]),
       appBar: AppBar(
         title: Image.asset("assets/images/fage-logo-white.png", scale: 2),
         centerTitle: true,
       ),
-      body: const Column(
-        children: [
-          CustomTextFormField(
-              labelText: "Pesquisar", prefixIcon: Icons.search),
-          SizedBox(height: 10),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Column(
-                  children: [
-                    CircleAvatar(
-                        backgroundColor: Colors.transparent,
-                        minRadius: 45,
-                        backgroundImage: AssetImage("assets/images/barbeiro.jpg")
-                    ),
-                    SizedBox(height: 10),
-                    Text("Barbearias")
-                  ],
-                ),
-                Column(
-                  children: [
-                    CircleAvatar(
-                        backgroundColor: Colors.transparent,
-                        minRadius: 45,
-                        backgroundImage: AssetImage("assets/images/barbeiro.jpg")
-                    ),
-                    SizedBox(height: 10),
-                    Text("Salões de\nbeleza", textAlign: TextAlign.center)
-                  ],
-                ),
-                Column(
-                  children: [
-                    CircleAvatar(
-                        backgroundColor: Colors.transparent,
-                        minRadius: 45,
-                        backgroundImage: AssetImage("assets/images/barbeiro.jpg")
-                    ),
-                    SizedBox(height: 10),
-                    Text("Saúde &\nBem-estar", textAlign: TextAlign.center,)
-                  ],
-                ),
-                Column(
-                  children: [
-                    CircleAvatar(
-                        backgroundColor: Colors.transparent,
-                        minRadius: 45,
-                        backgroundImage: AssetImage("assets/images/barbeiro.jpg")
-                    ),
-                    SizedBox(height: 10),
-                    Text("Spa &\nmassagem", textAlign: TextAlign.center,)
-                  ],
-                ),
-              ],
-            ),
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.transparent,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.favorite),
+            icon: Icon(Icons.favorite_outline),
+            label: "Meu Fage",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.search),
+            selectedIcon: Icon(Icons.search),
+            label: "Pesquisar",
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.calendar_today),
+            icon: Icon(Icons.calendar_today_outlined),
+            label: "Agendamentos",
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.person),
+            icon: Icon(Icons.person_outline),
+            label: "Perfil",
           ),
         ],
       ),
+      body: <Widget>[
+        const Column(
+          children: [
+            CustomTextFormField(labelText: "Pesquisar", prefixIcon: Icons.search),
+            SizedBox(height: 10),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          minRadius: 45,
+                          backgroundImage:
+                          AssetImage("assets/images/barbeiro.jpg")),
+                      SizedBox(height: 10),
+                      Text("Barbearias")
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          minRadius: 45,
+                          backgroundImage:
+                          AssetImage("assets/images/barbeiro.jpg")),
+                      SizedBox(height: 10),
+                      Text("Salões de\nbeleza", textAlign: TextAlign.center)
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          minRadius: 45,
+                          backgroundImage:
+                          AssetImage("assets/images/barbeiro.jpg")),
+                      SizedBox(height: 10),
+                      Text(
+                        "Saúde &\nBem-estar",
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          minRadius: 45,
+                          backgroundImage:
+                          AssetImage("assets/images/barbeiro.jpg")),
+                      SizedBox(height: 10),
+                      Text(
+                        "Spa &\nmassagem",
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Container(),
+        Container(),
+        Container(),
+      ][currentPageIndex],
     );
   }
 }
