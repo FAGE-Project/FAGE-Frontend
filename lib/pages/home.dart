b6aa96f1ade3d6beb6962cb67517d02ccfb7b788
Leonardo <leonardosarto1@hotmail.com>
2023-08-29 18:27:41 -0300

    Iniciado criação da tela inicial

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,16 +12,79 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: 0,
+          items: [
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.favorite_outline), label: "Meu Fage"),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.search), label: "Pesquisar"),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today_outlined),
+                label: "Agendamentos"),
+            const BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline), label: "Perfil"),
+          ]),
       appBar: AppBar(
         title: Image.asset("assets/images/fage-logo-white.png", scale: 2),
         centerTitle: true,
       ),
-      body: Column(
+      body: const Column(
         children: [
-          CustomTextFormField(labelText: "Pesquisar", prefixIcon: Icons.search),
-          CustomTextFormField(labelText: "Pesquisar", prefixIcon: Icons.search),
-          ListTile(
-            title: Text("Teste"),
+          CustomTextFormField(
+              labelText: "Pesquisar", prefixIcon: Icons.search),
+          SizedBox(height: 10),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                    CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        minRadius: 45,
+                        backgroundImage: AssetImage("assets/images/barbeiro.jpg")
+                    ),
+                    SizedBox(height: 10),
+                    Text("Barbearias")
+                  ],
+                ),
+                Column(
+                  children: [
+                    CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        minRadius: 45,
+                        backgroundImage: AssetImage("assets/images/barbeiro.jpg")
+                    ),
+                    SizedBox(height: 10),
+                    Text("Salões de\nbeleza", textAlign: TextAlign.center)
+                  ],
+                ),
+                Column(
+                  children: [
+                    CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        minRadius: 45,
+                        backgroundImage: AssetImage("assets/images/barbeiro.jpg")
+                    ),
+                    SizedBox(height: 10),
+                    Text("Saúde &\nBem-estar", textAlign: TextAlign.center,)
+                  ],
+                ),
+                Column(
+                  children: [
+                    CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        minRadius: 45,
+                        backgroundImage: AssetImage("assets/images/barbeiro.jpg")
+                    ),
+                    SizedBox(height: 10),
+                    Text("Spa &\nmassagem", textAlign: TextAlign.center,)
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
