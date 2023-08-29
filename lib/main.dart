143cbfc086531d19b05b3e3d4ae3b820fea69cf6
Helon21 <helonbentes51@hotmail.com>
2023-08-29 19:43:45 -0300

    Concluindo refatoração da tela, deixando responsiva com menos código

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:fage_telas/cadastro_empresa.dart';
-import 'package:fage_telas/cadastro_user.dart';
+
 import 'package:fage_telas/corpEdit.dart';
 import 'package:fage_telas/login.dart';
+import 'package:fage_telas/cadastroUser.dart';
 import 'package:fage_telas/userEdit.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Cadastro());
+  runApp(cadastroUser());
 }
